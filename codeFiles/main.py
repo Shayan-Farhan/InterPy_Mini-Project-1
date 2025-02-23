8abfd34d7d260df3eefa3f458ba215b64858f1af
safarhan25 <safarhan025@gmail.com>
2025-02-23 17:31:33 -0600

    Rearranged data to a list storing houses by decade built (for a bar chart)

--- codeFiles/main.py
+++ codeFiles/main.py
@@ -9,17 +9,36 @@ def step1():
     print(dataframe.tail(4))
     print(dataframe.info())
 
+#function for taking a 2Dlist, and returning a list with the average of each row
+
 #sort the dataframe by year built
 dataframe.sort_values('yr_built', inplace=True)
 
-
 #extract data for price and years built
 prices = dataframe['price']
-yearsBuilt = dataframe['yr_built']
+housesByDecade = [dataframe[(dataframe['yr_built'] >= 1900) & (dataframe['yr_built'] < 1910)],
+                  dataframe[(dataframe['yr_built'] >= 1910) & (dataframe['yr_built'] < 1920)],
+                  dataframe[(dataframe['yr_built'] >= 1920) & (dataframe['yr_built'] < 1930)],
+                  dataframe[(dataframe['yr_built'] >= 1930) & (dataframe['yr_built'] < 1940)],
+                  dataframe[(dataframe['yr_built'] >= 1940) & (dataframe['yr_built'] < 1950)],
+                  dataframe[(dataframe['yr_built'] >= 1950) & (dataframe['yr_built'] < 1960)],
+                  dataframe[(dataframe['yr_built'] >= 1960) & (dataframe['yr_built'] < 1970)],
+                  dataframe[(dataframe['yr_built'] >= 1970) & (dataframe['yr_built'] < 1980)],
+                  dataframe[(dataframe['yr_built'] >= 1980) & (dataframe['yr_built'] < 1990)],
+                  dataframe[(dataframe['yr_built'] >= 1990) & (dataframe['yr_built'] < 2000)],
+                  dataframe[(dataframe['yr_built'] >= 2000) & (dataframe['yr_built'] < 2010)],
+                  dataframe[dataframe['yr_built'] >= 2010]
+                  ]
 
-#graph price vs. square footage
+decadesBuilt = []
+for i in range(len(housesByDecade)):
+    decadesBuilt.append(housesByDecade[i]['yr_built'])
+
+
+"""graph bar chart of price vs. year built
 fig, ax = plt.subplots()
 ax.set(xlabel="Year Built", ylabel="Price", title="Price vs. Year Built")
 ax.grid()
 ax.plot(yearsBuilt, prices)
-plt.show()
+#plt.show()
+"""
